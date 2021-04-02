14382b3b679c54cc50d7680485b86472e4c90308
Art Nasamran <cnasamran@ucsd.edu>
2021-04-02 11:36:08 -0700

    Modify functions to calculate avg Q30 per sequence.

--- q30.py
+++ q30.py
@@ -5,45 +5,52 @@ import fastq
 import time
 
 def qual_stat(qstr):
-    q20 = 0
-    q30 = 0
+    """ Modified to calculate average quality score of a sequence """
+    q30_sum = 0
+    q30_seq = 0
+    # q30 = 0
     for q in qstr:
-        qual = ord(q) - 33
-        if qual >= 30:
-            q30 += 1
-            q20 += 1
-        elif qual >= 20:
-            q20 += 1
-    return q20, q30
+        # qual = ord(q) - 33
+        qual = q - 33
+        q30_sum += qual
+        # if qual >= 30:
+        #     q30 += 1
+    q30_seq = q30_sum/len(qstr)
+    return q30_seq
 
-def stat(filename):
+def stat(filename, output):
+    """ Modified to output total sequences and sequences with >=30 avg quality """
     reader = fastq.Reader(filename)
-    total_count = 0
-    q20_count = 0
-    q30_count = 0
+    # q30_count = 0
+    # total_count = 0
+    seq_count = 0
+    q30_seq_count = 0
     while True:
         read = reader.nextRead()
         if read == None:
             break
-        total_count += len(read[3])
-        q20, q30 = qual_stat(read[3])
-        q20_count += q20
-        q30_count += q30
+        # total_count += len(read[3])
+        q30_seq = qual_stat(read[3])
+        # q30_count += q30
+        seq_count += 1
+        if q30_seq >= 30:
+            q30_seq_count += 1
+    filename = str(filename)
+    total_seqs = "total sequences: " + str(seq_count)
+    q30_seqs = "q30 reads: " + str(q30_seq_count)
 
-    print("total bases:", total_count)
-    print("q20 bases:", q20_count)
-    print("q30 bases:", q30_count)
-    print("q20 percents:", 100 * float(q20_count)/float(total_count))
-    print("q30 percents:", 100 * float(q30_count)/float(total_count))
+    q30_out = open(output, "w")
+    q30_out.writelines([filename, "\n", total_seqs, "\n", q30_seqs])
+    q30_out.close()
+
 
 def main():
     if len(sys.argv) < 2:
-        print("usage: python q30.py <fastq_file>")
+        print("usage: python q30.py <fastq_file> <output_file>")
         sys.exit(1)
-    stat(sys.argv[1])
+    # stat(sys.argv[1])
+    stat(sys.argv[1], sys.argv[2])
+
 
 if __name__ == "__main__":
-    time1 = time.time()
     main()
-    time2 = time.time()
-    print('Time used: ' + str(time2-time1))
